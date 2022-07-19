05006670f8e1616581458f37d156808ffc1b3290
Appsorwebs <appsorwebs@gmail.com>
2022-07-19 16:42:58 -0700

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,3 +22,13 @@ class Square:
     def size(self):
         """Getter of the private attribute size"""
         return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter for the size private attribute"""
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
