cd9f68b32e95c759ea944a258645fdc8948cf004
Appsorwebs <appsorwebs@Appsorwebss-MacBook-Pro.local>
2022-08-21 01:09:49 +0100

    README

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Contains method lookup that returns list of object's attribute and methods
-"""
-
-
-def lookup(obj):
-    """returns list of object's attribute and methods"""
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Contains empty class BaseGeometry
-"""
-
-
-class BaseGeometry:
-    """empty class"""
-    pass
